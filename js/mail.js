0c39fd040478353b97e9691eb7b6fdeabcbdf0d3
Marko Nikolic <markoirmes@gmail.com>
2020-02-04 19:10:49 +0100

    Update contact form

---
+++ js/mail.js
@@ -0,0 +1,14 @@
+const senderemail = document.getElementById('email').value;
+const msg = document.getElementById('message').value;
+const sendername = document.getElementById('email').value;
+function sendEmail() {
+Email.send({
+    SecureToken : "fbe5b4e6-1fd9-4567-8b52-164d18706248",
+    To : 'markoirmes@gmail.com',
+    From : senderemail,
+    Subject : "New message for Webmark Studio",
+    Body : sendername + '<br><br>' + msg 
+}).then(
+  message => alert(message)
+);
+}
